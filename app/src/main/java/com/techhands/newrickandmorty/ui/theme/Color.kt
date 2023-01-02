64b4dcbf8d259157adb731abd6b9f0317df4a8de
dev-nick-patel <nikunjpatel.au@gmail.com>
2023-01-02 11:10:27 +1100

    - Splash screen implemented - Launcher icons added - Navigation implemented

--- app/src/main/java/com/techhands/newrickandmorty/ui/theme/Color.kt
+++ app/src/main/java/com/techhands/newrickandmorty/ui/theme/Color.kt
@@ -2,7 +2,13 @@ package com.techhands.newrickandmorty.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val PrimaryColor = Color(0xFF171c26)
+val PrimaryLightColor = Color(0xFF3e434e)
+val PrimaryDarkColor = Color(0xff000000)
+val SecondaryColor = Color(0xfffeb52c)
+val SecondaryLightColor = Color(0xffffe761)
+val SecondaryDarkColor = Color(0xFFc68500)
+val PrimaryTextColor = Color(0xFFffffff)
+val SecondaryTextColor = Color(0xFF000000)
+val PrimarySurfaceColor = Color(0xffFFFFFF)
+val SecondarySurfaceColor = Color(0xFFA36E03)
---
+++ app/src/main/java/com/techhands/newrickandmorty/utils/Constants.kt
@@ -0,0 +1,4 @@
+package com.techhands.newrickandmorty.utils
+
+const val SPLASH_SCREEN = "Splash_Screen"
+const val DASHBOARD_SCREEN = "Dashboard_Screen"
